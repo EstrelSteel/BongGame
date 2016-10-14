81feec50554c4b3f99cbce34b35f4432b02612b6
EstrelSteel <estrelsteel@gmail.com>
2016-10-14 15:23:15 -0600

    Collision and Score
    Added scores
    Added collision with paddles
    Added WIP type 1 ball

--- game3-test.js
+++ game3-test.js
@@ -1,5 +1,5 @@
-var WIDTH;
-var HEIGHT;
+var WIDTH = 400;
+var HEIGHT = 400;
 var start;
 var can;
 var ctx;
@@ -8,10 +8,14 @@ var balls = [];
 var p, b;
 var keys = [];
 var bgType = 0;
+var scores = [];
 
-function populateKeys() {
+function populateArrs() {
     for(var i = 0; i < 128; i++) {
         keys[i] = false;
+    }
+    for(var j = 0; j < balls.length + paddles.length; j++) {
+        scores[j] = 0;
     }
 }
 
@@ -33,14 +37,43 @@ function drawPaddles() {
     }
 }
 
-function calcNewBallPosition(b) {
+function calcNewBallPosition(bi) {
+    b = balls[bi];
     b.x = b.x + b.vx;
     b.y = b.y + b.vy;
     if(b.y + b.h >= HEIGHT || b.y <= 0) {
         b.vy = -b.vy;
+        if(b.y <= 0) {
+            b.y = 0;
+        }
+        else {
+            b.y = HEIGHT - b.h;
+        }
     }
     if(b.x + b.w >= WIDTH || b.x <= 0) {
         b.vx = -b.vx;
+        scores[bi] = scores[bi] + 1;
+         if(b.x <= 0) {
+            b.x = 0;
+        }
+        else {
+            b.x = WIDTH - b.w;
+        }
+    }
+    for(var i = 0; i < paddles.length; i++) {
+        p = paddles[i];
+        if((b.x >= p.x && b.x <= p.x + p.w) || (b.x < p.x && b.x + b.w >= p.x)) {
+            if((b.y >= p.y && b.y <= p.y + p.h) || (b.y < p.y && b.y + b.h >= p.y)) {
+                b.vx = -b.vx;
+                scores[i + balls.length] = scores[i + balls.length] + 1;
+                if(p.x > WIDTH / 2) {
+                    b.x = p.x - b.w;
+                }
+                else {
+                    b.x = p.x + p.w;
+                }
+            }
+        }
     }
     return b;
 }
@@ -49,13 +82,33 @@ function drawBalls() {
     for(var i = 0; i < balls.length; i++) {
         b = balls[i];
         if(start.start) {
-            b = calcNewBallPosition(b);
+            b = calcNewBallPosition(i);
         }
         switch(b.t) {
-        case 0:
+        case 0: //SQUARE
             ctx.fillRect(b.x, b.y, b.w, b.h);
             break;
+        case 1: //CIRCLE - WIP
+            ctx.arc(b.x, b.y, (b.w + b.h) / 4, 0, 2 * Math.PI);
         }
+    }
+}
+
+function drawScore() {
+    ctx.fillRect(0, HEIGHT, WIDTH, 1);
+    ctx.fillRect(WIDTH, 0, 1, HEIGHT);
+    var flavour;
+    for(var i = 0; i < scores.length; i++) {
+        flavour = "null";
+        if(i < balls.length) {
+            flavour = balls[i].n;
+            flavour = flavour + ": ";
+        }
+        else if(i < paddles.length + balls.length) {
+            flavour = paddles[i - balls.length].n;
+            flavour = flavour + ": ";
+        }
+        ctx.fillText(flavour + scores[i], 20, HEIGHT + (20 * (i + 1)));
     }
 }
 
@@ -71,41 +124,45 @@ function doKeyRelease(e) {
 }
 
 function clear() {
-	ctx.clearRect(0, 0, WIDTH, HEIGHT);
+	ctx.clearRect(0, 0, can.width, can.height);
 }
 
 function init() {
 	can = document.getElementById("canvas");
   	ctx = can.getContext("2d");
-  	WIDTH = can.width;
-  	HEIGHT = can.height;
+    if(WIDTH === 0 || HEIGHT === 0) {
+        WIDTH = can.width;
+        HEIGHT = can.height;
+    }
     start = {
         start: false, key: 32
     };
-    populateKeys();
     /*
     x = x-pos, y = y-pos, w = width, h = height, t = type,
         up = keycode up movement, down = keycode down movement
-        ms = move-speed
+        ms = move-speed, n = name
     */
     paddles[0] = {
         x: 20, y: HEIGHT / 2 - 50, w: 20, h: 100, t: 0,
-        up: 87, down: 83, ms: 5
+        up: 87, down: 83, ms: 5, n: "Left Paddle"
     };
     paddles[1] = {
         x: WIDTH - 40, y: HEIGHT / 2 - 50, w: 20, h: 100, t: 0,
-        up: 79, down: 76, ms: 5
+        up: 79, down: 76, ms: 5, n: "Right Paddle"
     };
     /*
     x = x-pos, y = y-pos, w = width, h = height, t = type,
         vx = velocity-x, vy = velocity-y,
         up = keycode up movement, down = keycode down movement
-        ms = move-speed, sc = speed-cap
+        ms = move-speed, sc = speed-cap, n = name;
     */
     balls[0] = {
         x: WIDTH / 2 - 10, y: HEIGHT / 2 - 10, w: 20, h: 20, t: 0, vx: 5, vy: 5,
-        up: 38, down: 40, ms: 1, sc: 10
+        up: 38, down: 40, ms: 1, sc: 10, n: "Ball"
     };
+    
+    populateArrs();
+    
     setInterval(tick, 30);
   	return setInterval(draw, 30);
 }
@@ -115,6 +172,7 @@ function draw() {
     drawBackground();
     drawPaddles();
     drawBalls();
+    drawScore();
 }
 
 function tick() {
